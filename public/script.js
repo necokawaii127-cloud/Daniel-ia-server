04edc8e4a3fe7cfbc6301f7eb24475a8c2663237
necokawaii127-cloud <necokawaii127@gmail.com>
2026-07-07 04:04:41 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -18,12 +18,12 @@ async function enviar() {
 
     // Indicador de escritura
     chat.innerHTML += `
-    <div class="daniel escribiendo" id="escribiendo">
-        <span></span>
-        <span></span>
-        <span></span>
-    </div>
-`;
+        <div class="daniel escribiendo" id="escribiendo">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    `;
 
     chat.scrollTop = chat.scrollHeight;
 
@@ -43,9 +43,26 @@ async function enviar() {
 
         document.getElementById("escribiendo").remove();
 
+        let respuesta = data.respuesta;
+
+        // Stickers
+        respuesta = respuesta.replace(/\[amoroso\]/gi, '<img src="img/amoroso.png" class="sticker">');
+        respuesta = respuesta.replace(/\[riendo\]/gi, '<img src="img/riendo.png" class="sticker">');
+        respuesta = respuesta.replace(/\[timido\]/gi, '<img src="img/timido.png" class="sticker">');
+        respuesta = respuesta.replace(/\[sueno\]/gi, '<img src="img/sueno.png" class="sticker">');
+        respuesta = respuesta.replace(/\[nervioso\]/gi, '<img src="img/nervioso.png" class="sticker">');
+        respuesta = respuesta.replace(/\[enojado\]/gi, '<img src="img/enojado.png" class="sticker">');
+        respuesta = respuesta.replace(/\[sonrojado\]/gi, '<img src="img/sonrojado.png" class="sticker">');
+        respuesta = respuesta.replace(/\[duda\]/gi, '<img src="img/duda.png" class="sticker">');
+        respuesta = respuesta.replace(/\[triste\]/gi, '<img src="img/triste.png" class="sticker">');
+        respuesta = respuesta.replace(/\[corazon\]/gi, '<img src="img/corazon.png" class="sticker">');
+        respuesta = respuesta.replace(/\[pena\]/gi, '<img src="img/pena.png" class="sticker">');
+        respuesta = respuesta.replace(/\[abrazo\]/gi, '<img src="img/abrazo.png" class="sticker">');
+        respuesta = respuesta.replace(/\[sopaipilla\]/gi, '<img src="img/sopaipilla.png" class="sticker">');
+
         chat.innerHTML += `
             <div class="daniel">
-                ${data.respuesta}
+                ${respuesta}
             </div>
         `;
 
