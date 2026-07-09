752a7b5a7101d3b0d769f051799197d6bba1ee1c
necokawaii127-cloud <necokawaii127@gmail.com>
2026-07-09 20:41:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -148,5 +148,5 @@ if (historial.length > 20) {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor iniciado en ${PORT}`);
-});
+  console.log("🚀 NUEVA VERSION DEL SERVIDOR");
+console.log(`Servidor iniciado en ${PORT}`);
